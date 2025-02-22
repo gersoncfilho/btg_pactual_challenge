28e9dc1d805b60c9399eb4f01c1f3f5107861ef2
GERSON CARDOSO FILHO <gerson_cardoso@hotmail.com>
2025-02-22 11:51:09 +0000

    implement OrderPersistenceAdapter to fulfill LoadOrderPort and SaveOrderPort interfaces

--- src/main/java/com/gersondeveloper/btg_challenge/OrderApplication/infrastructure/adapters/out/persistence/OrderPersistenceAdapter.java
+++ src/main/java/com/gersondeveloper/btg_challenge/OrderApplication/infrastructure/adapters/out/persistence/OrderPersistenceAdapter.java
@@ -1,4 +1,34 @@
 package com.gersondeveloper.btg_challenge.OrderApplication.infrastructure.adapters.out.persistence;
 
-public class OrderPersistenceAdapter {
+import com.gersondeveloper.btg_challenge.OrderApplication.application.ports.out.LoadOrderPort;
+import com.gersondeveloper.btg_challenge.OrderApplication.application.ports.out.SaveOrderPort;
+import com.gersondeveloper.btg_challenge.OrderApplication.domain.entities.Order;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
+public class OrderPersistenceAdapter implements
+        LoadOrderPort,
+        SaveOrderPort {
+    @Override
+    public BigDecimal getCustomerTotalOrderValue(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getCustomerOrdersList(int clientId) {
+        return List.of();
+    }
+
+    @Override
+    public int getCustomerOrdersCount(String customerId) {
+        return 0;
+    }
+
+    @Override
+    public void saveOrderMessage(String orderMessage) {
+
+    }
 }
